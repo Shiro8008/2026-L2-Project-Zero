edcb5a6c14020ec74b1f2a6f4decfeff032798b0
Shiro8008 <dottyjacson09@gmail.com>
2026-05-26 10:18:06 +1200

    Delete Practice directory

--- Practice/c_01_Make_Statement.py
+++
@@ -1,8 +0,0 @@
-# Functions goes here
-def make_statement(statement, decoration):
-    """Emphasises headings by adding decoration at the start and end"""
-
-    print(f"{decoration * 3} {statement} {decoration * 3}")
-
-# Main Routine Goes here
-make_statement(statement="programing is fun", decoration="💦")
