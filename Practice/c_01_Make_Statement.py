71de639d495d90fe2ec867c46270fe64e559aac4
Shiro8008 <dottyjacson09@gmail.com>
2026-05-26 10:18:50 +1200

    Add files via upload

---
+++ Practice/c_01_Make_Statement.py
@@ -0,0 +1,8 @@
+# Functions goes here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+# Main Routine Goes here
+make_statement(statement="programing is fun" , decoration="🔒")
